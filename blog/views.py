50eb94586c42d3a03bd370bf98155db7c547b7a0
Heisenberg <kiana726@Kianas-MacBook-Pro.local>
2020-11-30 19:53:29 +0330

    template

--- blog/views.py
+++ blog/views.py
@@ -17,9 +17,25 @@ class IndexPage(TemplateView):
                 'created_at': article.created_at.date(),
             })
 
+        promote_article_data= []
+        all_promote_article=Article.objects.filter(promote=True)
+        for promote_article in all_promote_article:
+            promote_article_data.append({
+                'title': promote_article.title,
+                'cover': promote_article.cover.url,
+                'category': promote_article.category.title,
+                'author': promote_article.author.user.first_name,
+                'author_cover': promote_article.author.avatar.url,
+                'created_at': promote_article.created_at.date()
+            })
+
         context = {
-            'article_data': article_data
+            'article_data': article_data,
+            'promote_article_data': promote_article_data,
         }
 
         return render(request,'index.html', context)
 
+
+class ContactPage(TemplateView):
+    template_name = "page-contact.html"
